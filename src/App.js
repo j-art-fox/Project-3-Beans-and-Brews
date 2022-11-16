cb43f51706e166a13e47120197fc076be49df7e5
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-11-15 21:17:46 -0700

    basic navbar and react router functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Nav/Navbar";
+import Footer from './components/Footer/Footer';
+import Home from './components/pages/Home';
+import Menu from './components/pages/Menu';
+import OrderOnline from './components/pages/OrderOnline';
+import OurCoffee from './components/pages/OurCoffee';
+import { Navigate, Switch, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar />
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/menu' element={<Menu/>} />
+        <Route path='/order' element={<OrderOnline/>} />
+        <Route path='/ourcoffee' element={<OurCoffee/>} />
+      </Routes>
     </div>
+    <Footer />
+  </>
   );
 }
 
