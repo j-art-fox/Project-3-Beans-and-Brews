4417cdd25512e7b658be0dcaedbc733ed2c2984f
Kyle Larsen <kylelarsen.business@gmail.com>
2022-11-27 13:55:16 -0700

    importing names and email from backend.

--- client/src/pages/Account/Rewards.jsx
+++ client/src/pages/Account/Rewards.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries.js';
 
-const Rewards = () => {
+function Rewards() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+  if (data) {
+    user = data.user;
+  }
+
   return (
     <div className="flex">
     <Sidebar/>
@@ -9,8 +17,16 @@ const Rewards = () => {
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <img className="lg:h-96 md:h-36 w-full object-cover object-center" src="https://images.pexels.com/photos/259165/pexels-photo-259165.jpeg" alt="blog"/>
                 <div className="p-6">
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">INSERT NAME</h1>
-                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">INSERTEMAIL@EMAIL.COM</h2>
+                  {user ? (
+                  <>
+                  <h1 className="title-font text-lg font-medium text-gray-900">
+                  {user.firstName} {user.lastName}
+                  </h1>
+                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-3">
+                  {user.email}
+                  </h2>
+                  </>
+                  ) : null}
                   <p className="leading-relaxed mb-3 mt-2 font-semibold text-blue-900">POINTS ACCRUED:
                   <span> No points yet!</span></p>
                   <div className="flex items-center flex-wrap">
