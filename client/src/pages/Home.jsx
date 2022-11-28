db7bb1c188c8ad5427505cb445b6395743fc899c
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-11-27 20:17:14 -0700

    feat: fixed mobile app links

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,9 +16,11 @@ const Home = () => {
       </section>
       <section className="flex flex-col justify-center items-center w-screen">
         <div className="my-10">
-          <h2 className="text-4xl font-bold border-4 border-black border-solid p-4 rounded-lg">Beans for All</h2>
+          <h2 className="text-4xl font-bold border-4 border-black border-solid p-4 rounded-lg">
+            Beans for All
+          </h2>
         </div>
-        <div className='container mx-auto grid grid-cols-1 grid-rows-3'>
+        <div className="container mx-auto grid grid-cols-1 grid-rows-3">
           <section className="box-border container mx-auto flex justify-center items-center mb-10">
             <div className="custom-home-card-1 h-80 p-6">
               <div className="flex flex-col justify-center w-full md:w-1/2 h-full p-4 text-white transition-all">
@@ -51,14 +53,18 @@ const Home = () => {
                 <div className="flex flex-col transition-all">
                   <a
                     className="flex flex-row items-center ease-in-out duration-300 hover:text-amber-600 font-bold"
-                    href="#"
+                    href="https://play.google.com/store/apps/details?id=com.beansandbrews.beansandbrews.android.app&hl=en_US&gl=US"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Download on Google Play
                     <FaChevronRight className="ml-2" />
                   </a>
                   <a
                     className="flex flex-row items-center ease-in-out duration-300 hover:text-amber-600 font-bold"
-                    href="#"
+                    href="https://apps.apple.com/us/app/beans-brews/id902029819"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Download on Apple Store
                     <FaChevronRight className="ml-2" />
@@ -70,9 +76,7 @@ const Home = () => {
           <section className="box-border container mx-auto flex justify-center items-center my-10">
             <div className="custom-home-card-3 h-80 p-6">
               <div className="flex flex-col justify-center w-full md:w-1/2 h-full p-4 text-white transition-all">
-                <h3 className="text-3xl font-bold my-1">
-                  Our Coffee.
-                </h3>
+                <h3 className="text-3xl font-bold my-1">Our Coffee.</h3>
                 <p className="text-xl my-1 custom-para">
                   There are two main species of coffee beans: Robusta and
                   Arabica. Since Robusta beans are cheaper to grow, they’re
