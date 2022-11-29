72ff6839785bc6b899057ec49ec89dfc79d53fc6
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-11-28 21:06:04 -0700

    feat: hamburgler menu functionality and style changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens: {
-      tiny: '375px',
-      xsm: '500px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      'tiny': '375px',
+      'xsm': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
       '2xl': '1536px'
     },
     extend: {
